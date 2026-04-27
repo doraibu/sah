4a0f8fb24953a59e621891dd2f98362f85c2444b
doraibu <f.santosbrito1@gmail.com>
2026-04-27 20:55:35 -0300

    cleaned comments

--- v1/sah.h
+++ v1/sah.h
@@ -8,7 +8,7 @@
  *
  * current status:
  * Linux	Ready
- * Windows	Ready
+ * NT	        Ready
  * OS X		Void
  *
  * @creator := Rafaes (doraibu)
@@ -16,75 +16,58 @@
 
 #ifdef __unix__
 
-/* LIB GLOBAL DEPENDENCIES ON LIBC */
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
-/* GLOBAL TYPES, MACROS AND VALUES */
 #define MEGABYTE (1024 * 1024)
+
 #define STACK_TINY MEGABYTE
 #define STACK_SMALL (2 * MEGABYTE)
 #define STACK_MEDIUM (4 * MEGABYTE)
 #define STACK_LARGE (8 * MEGABYTE)
 #define STACK_HUGE (16 * MEGABYTE)
+
 #define ALIGN(n) (((n) + 15) & ~15)
-#define MEM_POOL (64 * MEGABYTE)
-#define MEM_POOL2 (128 * MEGABYTE)
 
-/* BEGIN PUBLIC API TYPES SIGNATURE */
-
-// USED BY BASIC MODULE, INTERNAL HEADER FOR spop() TRACK SIZE
 struct _stack_header {
 	size_t size;
 };
 
-// USED BY BASIC MODULE, STACK CONTROLLER, ALLOCATED ON THE HEAP
 struct sah_stack {
 	size_t payload_size;
 	uint8_t* bp;
 	uint8_t* sp;
 }; __attribute__((aligned(64)));
 
-/* END PUBLIC API TYPES SIGNATURE */
+struct sah_stack* sah_create(size_t);
+void sah_destroy(struct sah_stack*);
+void* spush(struct sah_stack*, size_t);
+void spop(struct sah_stack*);
 
-/* BEGIN PUBLIC API FUNCTIONS SIGNATURE */
-
-struct sah_stack* screate(size_t); // USED BY BASIC MODULE, CREATES A STACK WITH A FIXED SIZE
-void sdestroy(struct sah_stack*); // USED BY BASIC MODULE, DESTROY A STACK
-void* spush(struct sah_stack*, size_t); // USED BY BASIC MODULE, PUSHES INTO THE STACK WITH A HEADER
-void spop(struct sah_stack*); // USED BY BASIC MODULE, POPS THE STACK ACCORDING TO spush() HEADER, NO NEED FOR MANUAL SIZE TRACKING
-
-/* END PUBLIC API FUNCTIONS SIGNATURE */
-
-/* BEGIN PUBLIC API STATIC FUNCTIONS SIGNATURE */
-
-// USED BY BASIC MODULE, PUSHES INTO THE STACK, REQUIRES MANUAL SIZE TRACKING
 static inline void* push(struct sah_stack* s, size_t n)
 {
 	s->sp -= n;
 	return s->sp;
 }
-// USED BY BASIC MODULE, POPS THE STACK, REQUIRES MANUAL SIZE TRACKING
+
 static inline void pop(struct sah_stack* s, size_t n)
 {
 	s->sp += n;
 }
-// USED BY BASIC MODULE, RESETS THE STACK POINTER BACK TO THE BASE POINTER
-static inline void sreset(struct sah_stack* s)
+
+static inline void sah_reset(struct sah_stack* s)
 {
 	s->sp = s->bp;
 }
-
-/* END PUBLIC API STATIC FUNCTIONS SIGNATURE */
 
 #ifdef SAH_IMPLEMENTATION
 
 #ifdef BASIC // BASIC MODULE
 
-struct sah_stack* screate(size_t psize)
+struct sah_stack* sah_create(size_t psize)
 {
 	size_t guard = sysconf(_SC_PAGESIZE);
 	size_t total = guard + psize;
@@ -107,7 +90,7 @@ struct sah_stack* screate(size_t psize)
 	return s;
 }
 
-void sdestroy(struct sah_stack* s)
+void sah_destroy(struct sah_stack* s)
 {
 	if (s == NULL) return;
 
@@ -142,11 +125,9 @@ void spop(struct sah_stack* s)
 
 #endif /* BASIC MODULE */
 
-#ifdef ASYNC // ASYNC MODULE
+#ifdef DYNAMIC
 
-/* W.I.P, MODULE FOR ASYNC, THREAD-SAFE, MULTITHREAD, GLOBAL, CONCURRENT AND PARALLEL STACKS */
-
-#endif /* ASYNC MODULE */
+#endif /* DYNAMIC MODULE */
 #endif /* IMPLEMENTATION */
 
 #endif /* __unix__ */
